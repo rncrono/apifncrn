c813c16d7e092298006de6d50182caf9f3f08938
Caio Rebert <caio.rebert@esig.com.br>
2022-05-16 21:21:18 -0300

    att

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://localhost:8000/*'
+        'http://localhost:8000/*',
+        'https://fncrn.com.br/*',
     ];
 }
